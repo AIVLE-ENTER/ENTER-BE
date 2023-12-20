7020b36bdbbc73369805648e8c6a125c6494e431
user <byhoon3@icloud.com>
2023-12-20 22:44:11 +0900

    Feat: ✨ Add Function (Send certification number mail)

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from . import views
+from . import signup_views
 
 app_name = "account"
 
 urlpatterns = [
-    path("signup/check-id/", views.check_id_duplicate, name="check-id"),
+    # signup_views.py
+    path("signup/check-id/", signup_views.check_id_duplicate, name="check-id"),
+    path(
+        "signup/email/",
+        signup_views.send_certification_number,
+        name="send-certification",
+    ),
 ]
--- account/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-from enter import models
-
-
-# 아이디 중복 체크 (중복시 사용불가, 중복되지 않을시 사용 가능 응답)
-def check_id_duplicate(request):
-    check_id = request.GET.get("id")
-    is_available = not models.Users.objects.filter(user_id=check_id).exists()
-    return JsonResponse({"is_available": is_available})
