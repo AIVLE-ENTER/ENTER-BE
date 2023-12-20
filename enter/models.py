19cba89136ac4b88d2a7116483a27e9fbc1a95e3
user <byhoon3@icloud.com>
2023-12-20 14:08:47 +0900

    Init: 🎉 Project Setup - Initialize project structure - Add .gitignore file - Create requirements.txt - Configure settings in settings.py - Define initial models in models.py

---
+++ enter/models.py
@@ -0,0 +1,115 @@
+from django.db import models
+
+
+class Analysismemo(models.Model):
+    memo_id = models.AutoField(primary_key=True)
+    analysis = models.ForeignKey('Analysisresults', on_delete=models.CASCADE)
+    comment_content = models.CharField(max_length=255)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    modified_datetime = models.DateTimeField(auto_now=True)
+    is_deleted = models.IntegerField(default=0) # 삭제여부 (0: 유지, 1: 삭제)
+
+    class Meta: 
+        managed = False
+        db_table = 'AnalysisMemo'
+
+
+class Analysisresults(models.Model):
+    analysis_id = models.AutoField(primary_key=True)
+    chat_window = models.ForeignKey('Chatwindow', on_delete=models.CASCADE)
+    request_message = models.CharField(max_length=255)
+    request_datetime = models.DateTimeField()
+    result_description = models.CharField()
+    result_imagepaths = models.CharField(max_length=255, blank=True, null=True)
+    result_datetime = models.DateTimeField()
+    use_memo = models.IntegerField(blank=True, null=True)  # 메모사용여부 (0: 미사용, 1: 사용)
+
+    class Meta:
+        managed = False
+        db_table = 'AnalysisResults'
+
+
+class Chatwindow(models.Model):
+    chat_window_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey('Users', on_delete=models.CASCADE)
+    target_object = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    crawling_text = models.CharField(max_length=255, blank=True, null=True)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    modified_datetime = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = False
+        db_table = 'ChatWindow'
+
+
+class Company(models.Model):
+    company_id = models.AutoField(primary_key=True)
+    company_name = models.CharField(max_length=50)
+    industry = models.CharField(max_length=50, blank=True, null=True)
+    number_of_employees = models.IntegerField(blank=True, null=True)
+    tot_sales = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Company'
+
+
+class Prompttemplates(models.Model):
+    template_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey('Users', on_delete=models.CASCADE)
+    template_name = models.CharField(max_length=30)
+    template_content = models.CharField(max_length=500)
+    created_datetime = models.DateTimeField(auto_now_add=True)
+    modified_datetime = models.DateTimeField(auto_now=True)
+    is_deleted = models.IntegerField(default=0) # 삭제여부 (0: 유지, 1: 삭제)
+
+    class Meta:
+        managed = False
+        db_table = 'PromptTemplates'
+
+
+class Qnaboard(models.Model):
+    board_id = models.AutoField(primary_key=True)
+    question_user = models.ForeignKey('Users', on_delete=models.CASCADE)
+    question_type = models.ForeignKey('Questiontype', on_delete=models.CASCADE)
+    question_title = models.CharField()
+    question_content = models.CharField()
+    question_file_name = models.CharField(max_length=255, blank=True, null=True)
+    question_file_path = models.CharField(max_length=255, blank=True, null=True)
+    question_datetime = models.DateTimeField()
+    answer_admin = models.ForeignKey('Users', on_delete=models.CASCADE, blank=True, null=True)
+    answer_content = models.CharField(max_length=255, blank=True, null=True)
+    answer_datetime = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'QnABoard'
+
+
+class Questiontype(models.Model):
+    question_type_id = models.AutoField(primary_key=True)
+    question_type_title = models.CharField(unique=True, max_length=255)
+    question_type_content = models.CharField(max_length=255)
+
+    class Meta:
+        managed = False
+        db_table = 'QuestionType'
+
+
+class Users(models.Model):
+    user_id = models.CharField(primary_key=True, max_length=20)
+    password = models.CharField(max_length=20)
+    user_name = models.CharField(max_length=20)
+    user_email = models.CharField(max_length=50)
+    kakao_id = models.CharField(max_length=255, blank=True, null=True)
+    naver_id = models.CharField(max_length=255, blank=True, null=True)
+    google_id = models.CharField(max_length=255, blank=True, null=True)
+    role = models.CharField(max_length=5, db_comment='admin: 관리자, user: 일반 사용자')
+    register_datetime = models.DateTimeField()
+    user_status = models.IntegerField(db_comment='0: 회원, 1: 탈퇴회원')
+    company = models.ForeignKey(Company, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'Users'
---
+++ enter/settings.py
@@ -0,0 +1,128 @@
+from pathlib import Path
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
+# BASE 폴더
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# secrets.json
+secret_file = os.path.join(BASE_DIR, 'secrets.json') 
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_env_variable(var_name):
+  try:
+    return secrets[var_name]
+  except KeyError:
+    error_msg = 'Set the {} environment variable'.format(var_name)
+    raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_env_variable("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "enter.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "enter.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_env_variable("MYSQL_NAME"),
+        'USER': get_env_variable("MYSQL_USER"),
+        'PASSWORD': get_env_variable("MYSQL_PASSWORD"),
+        'HOST': get_env_variable("MYSQL_HOST"),
+        'PORT': get_env_variable("MYSQL_PORT"),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = "static/"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
