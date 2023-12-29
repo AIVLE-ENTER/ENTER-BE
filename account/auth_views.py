90a440d6dac1961473215b4c064a9cc0e5fc8ce8
user <byhoon3@icloud.com>
2023-12-29 15:00:02 +0900

    Refactor: 🔨 Refactoring code related to sign out - Add Error Handling

--- account/auth_views.py
+++ account/auth_views.py
@@ -138,10 +138,20 @@ def chage_password(request):
 @csrf_exempt
 @require_POST
 def sign_out(request):
+    # 토큰 검증
+    user, response = validate_token(request)
+    if not response["success"]:
+        return JsonResponse(response, status=400)
+    
     # 데이터 받아오기
     json_data = json.loads(request.body.decode("utf-8"))
     user_id = json_data.get("user_id")
     password = json_data.get("password")
+    
+    # 필수 데이터 누락
+    if user_id is None or password is None:
+        response_data = {"success": False, "message": "오류: 필수 데이터 누락"}
+        return JsonResponse(response_data, status=400)
 
     # 사용자 확인
     user_params = {"user_id": user_id, "user_status": 0}
@@ -151,6 +161,11 @@ def sign_out(request):
     user = models.Users.objects.filter(**user_params)[0]
     if user.password != encode_sha256(password):
         response_data = {"success": False, "message": "비밀번호가 일치하지 않습니다."}
+        return JsonResponse(response_data, status=400)
+    
+    # 탈퇴 권한
+    if user_id != user.user_id:
+        response_data = {"success": False, "message": "잘못된 요청입니다. (로그인한 유저와 탈퇴하려는 유저가 다릅니다.)"}
         return JsonResponse(response_data, status=400)
 
     # 탈퇴
@@ -162,8 +177,8 @@ def sign_out(request):
 
 # 사용자 정보
 def user_info(request):
+    # 토큰 검증
     user, response = validate_token(request)
-
     if not response["success"]:
         return JsonResponse(response, status=400)
 
