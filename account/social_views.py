e55d3d80ea7947e4d4b6fa5b85e6e5dea67df4dd
user <byhoon3@icloud.com>
2024-01-04 12:01:02 +0900

    Refactor: 🔨 Refactoring code related to google login - correct redirect uri - correct receive code

--- account/social_views.py
+++ account/social_views.py
@@ -35,7 +35,6 @@ def kakao_login(request):
     if not user_info_json:
         error_message = {"message": "유저 정보를 받아오지 못했습니다."}
         return JsonResponse(error_message, status=400)
-    print(user_info_json)
 
     # 회원가입 및 로그인
     kakao_id = user_info_json.get("id")
@@ -45,76 +44,92 @@ def kakao_login(request):
         return JsonResponse(response_data, status=400)
 
     if not models.Users.objects.filter(kakao_id=kakao_id).exists():
-        response_data = {"success": True, "message": "not exists", "data": {"id": kakao_id}}
+        response_data = {
+            "success": True,
+            "message": "not exists",
+            "data": {"id": kakao_id},
+        }
         return JsonResponse(response_data, status=200)
 
     # jwt 토큰 발급하여 로그인
     user = models.Users.objects.get(kakao_id=kakao_id)
-    print(user.user_id)
     token = create_token(user.user_id)
-    
+
     response_data = {"success": True, "message": "exists", "data": {"token": token}}
     return JsonResponse(response_data, status=200)
 
 
-#구글 로그인
-
+# 구글 로그인
 @csrf_exempt
 @require_POST
 def google_login(request):
-    
     CLIENT_ID = get_env_variable("CLIENT_ID")
     CLIENT_SECRET = get_env_variable("CLIENT_SECRET")
 
     # 클라이언트에서 받은 인가 코드
-    authorization_code = request.POST.get('code')
+    json_data = json.loads(request.body.decode("utf-8"))
+    authorization_code = json_data.get("code")
 
     # 로그인 토큰을 얻기 위한 요청 설정
-    token_url = 'https://oauth2.googleapis.com/token'
+    token_url = "https://oauth2.googleapis.com/token"
     token_payload = {
-        'code': authorization_code,
-        'client_id': CLIENT_ID,
-        'client_secret': CLIENT_SECRET,
-        'redirect_uri': 'http://localhost:5500/signup_test.html?type=google',
-        'grant_type': 'authorization_code'
+        "code": authorization_code,
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET,
+        "redirect_uri": "http://localhost:5500/signin_test.html?type=google",
+        "grant_type": "authorization_code",
     }
 
     # 로그인 토큰 요청
     token_response = requests.post(token_url, data=token_payload)
+    print(token_response.json())
 
     if token_response.status_code == 200:
-        access_token = token_response.json().get('access_token')
+        access_token = token_response.json().get("access_token")
 
         # 구글 사용자 정보 요청
-        user_info_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
+        user_info_url = "https://www.googleapis.com/oauth2/v1/userinfo"
         headers = {
-            'Authorization': f'Bearer {access_token}',
+            "Authorization": f"Bearer {access_token}",
         }
-        user_info_response = requests.get(user_info_url, headers=headers) # 여기까지가 response 받아오는 코드.
+        user_info_response = requests.get(
+            user_info_url, headers=headers
+        )  # 여기까지가 response 받아오는 코드.
 
         if user_info_response.status_code == 200:
-            google_id = user_info_response.json()['id']
-            
+            google_id = user_info_response.json()["id"]
+
             if not google_id:
                 response_data = {"success": False, "message": "구글 계정을 받아오지 못했습니다."}
                 return JsonResponse(response_data, status=400)
 
             if not models.Users.objects.filter(google_id=google_id).exists():
                 response_data = {
-                    "success": True, 
-                    "message": "not exists", 
-                    "data": {"id": google_id}
+                    "success": True,
+                    "message": "not exists",
+                    "data": {"id": google_id},
                 }
                 return JsonResponse(response_data, status=200)
 
             # jwt 토큰 프론트로 전달
             user = models.Users.objects.get(google_id=google_id)
             token = create_token(user.user_id)
-            response_data = {"success": True, "message": "exists", "data": {"token": token}}         
+            response_data = {
+                "success": True,
+                "message": "exists",
+                "data": {"token": token},
+            }
             return JsonResponse(response_data, status=200)
 
         else:
-            return JsonResponse({"success": False, 'message': 'Failed to fetch user info from Google'}, status=500)
-        
+            return JsonResponse(
+                {"success": False, "message": "Failed to fetch user info from Google"},
+                status=500,
+            )
+
     else:
-        return JsonResponse({"success": False, "message": "access token을 받아오는데 실패했습니다."}, status=500, json_dumps_params={"ensure_ascii": False},)
+        return JsonResponse(
+            {"success": False, "message": "access token을 받아오는데 실패했습니다."},
+            status=500,
+            json_dumps_params={"ensure_ascii": False},
+        )
