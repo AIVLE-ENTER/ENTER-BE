ed67aa85c3d5e34930877d5bdc2a5f45dbcde606
user <byhoon3@icloud.com>
2024-01-07 22:33:15 +0900

    Refactor: 🔨 Refactoring code (send certification number) - if purpose is findPW,  send email only when matching member information (email)

--- utils/views.py
+++ utils/views.py
@@ -26,23 +26,31 @@ def send_certification_number(request):
     certification_number = random.randint(100000, 999999)
     content = template[0].content.format(certification_number=certification_number)
 
-    # 이메일 전송
-    is_send = send_email(title, content, email)
-    if not is_send["success"]:
-        response_data = {
-            "success": False,
-            "message": f"이메일 전송에 실패했습니다. 오류 메시지: {is_send['message']}",
-        }
-        return JsonResponse(response_data, status=500)
+    # 비밀번호 찾기의 경우 회원 정보의 이메일과 일치할 경우에만 전송
+    if purpose == "findPW":
+        user_id = json_data.get("user_id")
+        if not models.Users.objects.filter(user_email=email, user_id=user_id).exists():
+            response_data = {"success": False, "message": "이메일이 회원정보와 일치하지 않습니다."}
+            return JsonResponse(response_data, status=200)
 
-    # 인증번호 저장
-    is_save = save_email_auth(email, certification_number, purpose)
-    if not is_save["success"]:
-        response_data = {
-            "success": False,
-            "message": f"이메일 인증을 다시 시도해주세요. 오류 메시지: {is_save['message']}",
-        }
-        return JsonResponse(response_data, status=500)
+    if send_email:
+        # 이메일 전송
+        is_send = send_email(title, content, email)
+        if not is_send["success"]:
+            response_data = {
+                "success": False,
+                "message": f"이메일 전송에 실패했습니다. 오류 메시지: {is_send['message']}",
+            }
+            return JsonResponse(response_data, status=500)
+
+        # 인증번호 저장
+        is_save = save_email_auth(email, certification_number, purpose)
+        if not is_save["success"]:
+            response_data = {
+                "success": False,
+                "message": f"이메일 인증을 다시 시도해주세요. 오류 메시지: {is_save['message']}",
+            }
+            return JsonResponse(response_data, status=500)
 
     response_data = {"success": True, "message": "이메일 전송이 성공적으로 완료되었습니다."}
     return JsonResponse(response_data, status=200)
